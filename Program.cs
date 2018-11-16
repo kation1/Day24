9f077840c194ea6ad8c761f8661ca493d9a78777
Kation <Klpidek@gmail.com>
2018-11-15 23:11:16 -0500

    Day 24

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// private static List<int> _dataList = new List<int>();
+public static Node removeDuplicates(Node head)
+{
+
+	if (head == null || head.next == null)
+	{
+		return head;
+	}
+	if (head.data == head.next.data)
+	{
+		head.next = head.next.next;
+		removeDuplicates(head);
+	}
+	if (head != head.next)
+
+	{
+		removeDuplicates(head.next);
+	}
+
+	return head;
+
+}
